eb3730f1c951fd6c8819423e394ad60fad12ba0e
sidra.sm18@gmail.com <sidra.sm18@gmail.com>
2019-03-20 18:52:49 +0530

    Last commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,69 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  @ViewChild('form') formRef:NgForm; //using view child since iam not passing the form varibale inside the method
+  defaultQuestion = 'pet'; //it is a value available from the options
+  answer ='';
+  genders = ['male','female','other'];
+  user={
+    userName:'',
+    userEmail:'',
+    secretQuestion:'',
+    answer:'',
+    gender:''
+  }
+  submitted = false;
+  sugegstedName="superName"
+
+  onSuggest()
+  {
+    // //seting a value
+    this.formRef.setValue(
+      {
+        userData:{
+          username:this.sugegstedName,
+          email:'suggest@gmail.com'
+        },
+        secret:'pet',
+        questionAnswer:'nelly',
+        gender:'female'
+      }
+    );
+ 
+
+    //patch value
+    // this.formRef.form.patchValue(
+    //   {
+    //     userData:
+    //     {
+    //       username:this.sugegstedName
+    //     }
+    //   }
+    // );
+    }
+
+  // onSubmit(form:NgForm)
+  // {
+  //   console.log(form);
+  // }
+
+  onSubmit()
+  {
+    this.submitted=true;
+    this.user.userName = this.formRef.value.userData.username;
+    this.user.userEmail = this.formRef.value.userData.email;
+    this.user.secretQuestion = this.formRef.value.secret;
+    this.user.answer = this.formRef.value.questionAnswer;
+    this.user.gender = this.formRef.value.gender;
+    this.formRef.reset();
+
+    console.log(this.formRef);
+  }
+  onReset()
+  {
+    this.formRef.reset();
+  }
 }
+
+
